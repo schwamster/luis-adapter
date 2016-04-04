57d781a08473b5e5f171c67cf612ea0aec9a92fb
schwamster <bastian.toepfer@bodoconsult.de>
2016-04-04 22:58:10 +0200

    more setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # luis
+
+work in progress
---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+describe('luis-adapter', function() {
+  describe('init', function () {
+    it('should return the adapter with api-id and subscriptionid', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
