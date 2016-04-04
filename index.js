6198d5f7272e03e526adceb4f70dbdef4bc26f55
schwamster <bastian.toepfer@bodoconsult.de>
2016-04-05 00:34:18 +0200

    update

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-module.exports = function(width, height) {
-  return width * height;
+var request = require("request");
+
+function LuisAdapter(options) {
+  this.appId = options.appId;
+  this.subscriptionKey = options.subscriptionKey;
+}
+
+LuisAdapter.prototype.Query = function() {
+  var x = request("http://www.sitepoint.com", function(error, response, body) {
+    console.log("lasdfas");
+  });
 };
+
+LuisAdapter.prototype.AppId = function() {
+    return this.appId;
+};
+
+LuisAdapter.prototype.SubscriptionKey = function() {
+    return this.subscriptionKey;
+};
+
+module.exports = LuisAdapter;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,30 @@
 var assert = require('assert');
+
 describe('luis-adapter', function() {
   describe('init', function () {
     it('should return the adapter with api-id and subscriptionid', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+
+      var options = {};
+      options.appId = "abc";
+      options.subscriptionKey = "123"
+      var LuisAdapter = require("../index");
+      var luisAdapter = new LuisAdapter(options);
+
+      assert.equal(options.appId, luisAdapter.AppId());
+      assert.equal(options.subscriptionKey, luisAdapter.SubscriptionKey());
+    });
+  });
+
+  describe('query', function () {
+    it('query with valid appid and subscriptionKey', function () {
+
+      var options = {};
+      options.appId = "abc";
+      options.subscriptionKey = "123"
+      var LuisAdapter = require("../index");
+      var luisAdapter = new LuisAdapter(options);
+
+      assert.equal("some answer", luisAdapter.Query());
     });
   });
 });
